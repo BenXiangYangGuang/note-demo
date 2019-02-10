7908fe9aa427c2239dc92c85b2050d18e99c25a8
wewe <987050838@qq.com>
2019-02-10 16:12:45 +0800

    观察者模式

--- src/main/java/com/wewe/designpattern/observerParttern/client/Client1.java
+++ src/main/java/com/wewe/designpattern/observerParttern/client/Client1.java
@@ -4,12 +4,13 @@ package com.wewe.designpattern.observerParttern.client;
 import com.wewe.designpattern.observerParttern.observer.ITalent;
 import com.wewe.designpattern.observerParttern.observer.JuniorEngineer;
 import com.wewe.designpattern.observerParttern.observer.Architect;
-import com.wewe.designpattern.observerParttern.observer.ITalent;
-import com.wewe.designpattern.observerParttern.observer.JuniorEngineer;
 import com.wewe.designpattern.observerParttern.observer.SeniorEngineer;
 import com.wewe.designpattern.observerParttern.subject.AbstractHR;
 import com.wewe.designpattern.observerParttern.subject.HeadHunter;
 
+/**
+ * 观察者模式:一个主题发生了变化,自动通知每一个观察者做update(),实现不同的观察者操作
+ */
 public class Client1 {
 
   public static void main(String[] args) {
